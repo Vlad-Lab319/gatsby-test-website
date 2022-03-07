799bf007615304c8a39221ec5ba122a5573756ed
vlad-lab319 <solo15mag@gmail.com>
2022-03-07 16:02:42 -0500

    Add mdx plugin and fetch data from mdx files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 
 const BlogPage = ({ data }) => {
@@ -8,10 +9,20 @@ const BlogPage = ({ data }) => {
     <Layout pageTitle="My Blog Posts">
       <ul>
         {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name} {node.changeTime}
-            </li>
+          data.allMdx.nodes.map(node => (
+
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>Modified: {node.parent.modifiedTime}</p>
+              <MDXRenderer>
+                {node.body}
+              </MDXRenderer>
+            </article>
+
+            // <li key={node.name}>
+            //   {node.name} {node.changeTime}
+            // </li>
           ))
         }
       </ul>
@@ -19,15 +30,36 @@ const BlogPage = ({ data }) => {
   );
 }
 
+// export const query = graphql`
+//   query MyQuery {
+//     allFile {
+//       nodes {
+//         name
+//         changeTime
+//       }
+//     }
+//   }
+// `
+
 export const query = graphql`
-  query MyQuery {
-    allFile {
-      nodes {
-        name
-        changeTime
+query MyQuery {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      id
+      body
+      parent {
+        ... on File {
+          modifiedTime(formatString: "MMMM D, YYYY")
+        }
       }
     }
   }
+}
 `
+
 
 export default BlogPage;
